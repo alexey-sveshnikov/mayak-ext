e79b02bff4981d6ba1617fef7964fa38a7f928e1
Alexey Sveshnikov <alexey.sveshnikov@gmail.com>
2022-10-16 01:47:47 +0300

    parse table data with quantities

---
+++ src/parsing/helpers.ts
@@ -0,0 +1,13 @@
+export function doXpath(query: string, root: HTMLElement, resultType = XPathResult.ORDERED_NODE_ITERATOR_TYPE): HTMLElement[] {
+  const result: HTMLElement[] = []
+  const xp = document.evaluate('.//tr', root, null, XPathResult.ORDERED_NODE_ITERATOR_TYPE)
+
+  let item = xp.iterateNext()
+  while (item) {
+    if (item instanceof HTMLElement) {
+      result.push(item)
+    }
+    item = xp.iterateNext()
+  }
+  return result
+}
