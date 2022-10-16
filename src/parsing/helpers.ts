9f7f236ddae2e2cccff3d4b950222cbc4400c1c8
Alexey Sveshnikov <a.sveshnikov@softpro.com>
2022-10-16 04:23:46 +0300

    remove unused code

--- src/parsing/helpers.ts
+++ src/parsing/helpers.ts
@@ -1,6 +1,6 @@
 export function doXpath(query: string, root: HTMLElement, resultType = XPathResult.ORDERED_NODE_ITERATOR_TYPE): HTMLElement[] {
   const result: HTMLElement[] = []
-  const xp = document.evaluate('.//tr', root, null, XPathResult.ORDERED_NODE_ITERATOR_TYPE)
+  const xp = document.evaluate(query, root, null, XPathResult.ORDERED_NODE_ITERATOR_TYPE)
 
   let item = xp.iterateNext()
   while (item) {
