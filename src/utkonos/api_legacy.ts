3e7714feeb7c1c60d11a55b779a820fe725843e6
Alexey Sveshnikov <a.sveshnikov@softpro.com>
2022-10-29 21:54:05 +0300

    adm.utkonos.ru support

--- src/utkonos/api_legacy.ts
+++ src/utkonos/api_legacy.ts
@@ -62,7 +62,7 @@ class UtkonosLegacyAPI {
     const request = {
       "Head": {
         "DeviceId": deviceData['device_id'],
-        "Domain": "www.utkonos.ru",
+        "Domain": "adm.utkonos.ru",
         "RequestId": uuidv4().replaceAll(/-/g, ''),
         "MarketingPartnerKey": "mp-cc3c743ffd17487a9021d11129548218",
         "Version": "utkonos-ext",
@@ -77,7 +77,7 @@ class UtkonosLegacyAPI {
     const formData = new FormData()
     formData.append("request", JSON.stringify(request))
 
-    const response = await fetch(`https://www.utkonos.ru/api/rest/${method}`, {
+    const response = await fetch(`https://adm.utkonos.ru/api/rest/${method}`, {
       "headers": {
         "accept": "application/json, text/plain, */*",
         "accept-language": "en-US,en;q=0.9,ru;q=0.8",
@@ -90,7 +90,7 @@ class UtkonosLegacyAPI {
         "sec-fetch-mode": "cors",
         "sec-fetch-site": "same-origin"
       },
-      "referrer": "https://www.utkonos.ru/",
+      "referrer": "https://adm.utkonos.ru/",
       "referrerPolicy": "no-referrer-when-downgrade",
       "body": formData,
       "method": "POST",
